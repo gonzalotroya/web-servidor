639d640a8a24039e19f04ccf05b4662ebb9735fe
gonzalotroya <92518256+gonzalotroya@users.noreply.github.com>
2022-10-14 11:16:51 +0200

    contenidos3

--- 02validacionformulario/videojuegos/index.php
+++ 02validacionformulario/videojuegos/index.php
@@ -11,13 +11,51 @@
     <h2>NUevo videojuego</h2>
     <?php
     if($_SERVER["REQUEST_METHOD"]=="POST"){
-        echo depurar($_POST["titulo"]);
-        echo depurar($_POST["precio"]);
+        $temp_titulo= depurar($_POST["titulo"]);
+        $temp_precio =depurar($_POST["precio"]);
+        $temp_consola=depurar($_POST["consola"]);
+
         if (empty($temp_titulo)) {
             $err_titulo="El campo es obligario";
+        }else {
+            if(strlen($temp_titulo)>40){
+                $err_titulo="Demasiado largo";
+            }else {
+                $titulo=$temp_titulo;
+            }
         }
+
+        if (empty($temp_consola)) {
+            $err_consola="El campo es obligario";
+        }else {
+           if(empty($temp_consola)){
+                $consola=$temp_consola;
+           }
+            
+        }
+
         if (empty($temp_precio)) {
             $err_precio="El campo es obligario";
+        }else {
+            $temp_precio=round($temp_precio,2);
+            $temp_precio=filter_var($temp_precio,FILTER_VALIDATE_FLOAT);
+            if(!$temp_precio){
+                $err_precio="El precio debe ser un numero";
+            }else {
+                if($temp_precio<0){
+                    $err_precio="El precio no puede ser negativo";
+                }elseif ($temp_precio>=10000) {
+                    $err_precio="El precio no puede ser igual o superior a 10000";
+                }else {
+                    $precio=$temp_precio;
+                }
+            }
+        }
+        if(isset($titulo)&& isset($precio)&& isset($consola)){
+            echo "<p>$titulo</p>";
+            echo "<p>$precio</p>";
+            echo "<p>$consola</p>";
+
         }
     }
 
@@ -38,6 +76,18 @@
        <p> Precio: <input type="text" name="precio">
        <span class="error">
         * <?php if(isset($err_precio)) echo $err_precio ?>
+       </span>
+       </p>
+
+       <p> Consola: <select name="consola">
+    <option disabled selected value> -- select an option -- </option>
+    <option value="ps4">ps4</option>
+    <option value="xbox">xbox</option>
+    <option value="pc">pc</option>
+    <option value="Switch">Switch</option>
+    </select>
+       <span class="error">
+        * <?php if(isset($err_consola)) echo $err_consola ?>
        </span>
        </p>
        <p><input type="submit" name="Crear"></p>
